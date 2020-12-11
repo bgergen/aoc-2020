b68a382679139086422aea7882cc80d4569adc20
Bryan Gergen <bryan.gergen@nytimes.com>
2020-12-11 00:16:51 -0500

    Day8 JS Part2

--- day08/js/index.js
+++ day08/js/index.js
@@ -4,11 +4,12 @@ const input = getInputString(__dirname)
   .split('\n')
   .map(row => row.split(' '));
 
-function findInfiniteLoop(instructions) {
+function findInfiniteLoop(instructions, isFix) {
   let acc = 0;
   let i = 0;
   const instructionsRun = new Set();
   while (!instructionsRun.has(i)) {
+    if (i === instructions.length) return acc;
     instructionsRun.add(i);
     const [cmd, val] = instructions[i];
     switch (cmd) {
@@ -24,7 +25,33 @@ function findInfiniteLoop(instructions) {
         throw new Error('Invalid command');
     }
   }
-  return acc;
+  return isFix ? null : acc;
 }
 
+const canFlip = {
+  nop: 'jmp',
+  jmp: 'nop',
+};
+
+function fixInfiniteLoop(instructions, idx = 0) {
+  while (!canFlip[instructions[idx][0]]) {
+    idx++;
+  }
+
+  const [cmd, val] = instructions[idx];
+  const newInstr = [
+    ...instructions.slice(0, idx),
+    [canFlip[cmd], val],
+    ...instructions.slice(idx + 1),
+  ]
+
+  const acc = findInfiniteLoop(newInstr, true);
+  if (acc !== null) return acc;
+
+  return fixInfiniteLoop(instructions, idx + 1);
+}
+
+// Part 1: 1394
 console.log(findInfiniteLoop(input));
+// Part 2: 1626
+console.log(fixInfiniteLoop(input));
