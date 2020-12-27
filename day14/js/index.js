75bdefd57c0dcf52e72e4312e1259a596a55b06f
Bryan Gergen <bryan.gergen@nytimes.com>
2020-12-27 18:18:25 -0500

    Day14 JS Part 2 Attempt

--- day14/js/index.js
+++ day14/js/index.js
@@ -4,27 +4,55 @@ const input = getInputString(__dirname)
   .split('\n')
   .map(r => r.split('=').map(s => s.trim()));
 
-function run(program) {
+function setVal(mem, addr, val) {
+  if (!addr.includes('X')) {
+    mem[parseInt(addr, 2)] = val;
+    return;
+  }
+  for (const r of ['0', '1']) {
+    setVal(mem, addr.replace('X', r), val);
+  }
+}
+
+function run(program, v1 = true) {
   const mem = [];
   let mask;
 
   program.forEach(([instr, val]) => {
     if (instr === 'mask') {
-      mask = val.split('').reduce((maskObj, char, i) => {
-        if (char === 'X') return maskObj;
-        return { ...maskObj, [i]: char };
-      }, {});
+      mask = val;
     } else {
       const addr = Number(instr.match(/\d+/));
-      const bin = Number(val).toString(2).padStart(36, '0').split('');
-      Object.keys(mask).forEach(i => {
-        bin[i] = mask[i];
-      });
-      mem[addr] = parseInt(bin.join(''), 2);
+      if (v1) {
+        const bin = Number(val).toString(2).padStart(36, '0').split('');
+        for (let i = 0; i < mask.length; i++) {
+          if (mask[i] !== 'X') {
+            bin[i] = mask[i];
+          }
+        }
+        mem[addr] = bin.join('');
+      } else {
+        // There's a bug in here somewhere, but I know not where.
+        const bin = addr.toString(2).padStart(36, '0').split('');
+        for (let i = 0; i < mask.length; i++) {
+          if (mask[i] !== '0') {
+            bin[i] = mask[i];
+          }
+        }
+        setVal(mem, bin.join(''), Number(val));
+      }
     }
   });
 
-  return mem.reduce((sum, val) => sum + val, 0);
+  return mem.reduce((sum, val) => {
+    if (!val) return sum;
+    return v1
+      ? sum + parseInt(val, 2)
+      : sum + val;
+  }, 0);
 }
 
+// Part 1: 6559449933360
 console.log(run(input));
+// Part 2: ???
+console.log(run(input, false));
